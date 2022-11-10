3d104c80c550c641b5dba1cd33b8f863188a5d62
Sricharan7113 <90692839+Sricharan7113@users.noreply.github.com>
2022-11-10 23:59:31 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+debugger;
+function Change(){
+    var C = document.getElementById('Para');
+    C.style.backgroundColor="#2d5481";
+    C.style.color="White";
+    C.style.border=" 2px dotted white";
+}
